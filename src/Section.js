1162b4840f1ba90aab841dc6aaace40fcd7c21e4
krkmariia <61343459+krkmariia@users.noreply.github.com>
2021-02-26 21:34:06 +0200

    Delete Section.js

--- src/Section.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Weather from "./Weather";
-
-export default function Section({name, children}) {
-    return (<>
-    <h1>{name}</h1>
-    {children}
-    </>
-)};
